677a3c05510cdccb7b8f0e9d12153834eb3eaf0b
Martin Rydén <pemryd@gmail.com>
2017-12-12 23:37:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,244 +1,100 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 21:42:18 2017
+
+@author: martin
+"""
 
 import pandas as pd
-
-class data(object):
-    filename = 'acacia.csv'
-    
-    # Read csv as dataframe
-    df = pd.read_csv(filename, sep=';')
-    
-    # Variable name for statistical tests
-    varname = filename.split('.')[0]
-
-# Initialize dictionary with column names
-# This dict will hold the datatype for each column
-dt = {c:'' for c in data.df.columns}
-
-# Display 5 initial rows
-def overview(df):
-    return df.head()
-
-# Display number of rows and columns
-def dimensions(df):
-    nr_rows     = df.shape[0]
-    nr_columns  = df.shape[1]
-    return (nr_rows, nr_columns)
-
-# Count and remove rows with null values
-def null_values(df):
-    # Columns where empty values are present
-    null_vals = [column for column in df.columns if df[column].isnull().values.any()]
-    
-    # If any column with empty values exists
-    if len(null_vals) > 0:
-        print('Null values found in: \n%s' % ('\n'.join(null_vals)))
-        print('\nRemoving rows with empty values...')
         
-        # Remove rows with null values
-        df = df.dropna(how='any')
-        
-        # Recheck dimensions
-        print( dimensions(df), '(updated)')
-        
-        return df # Return updated dataframe
-        
-    else:
-        print('No null values found.\n')
-        return df # Return unchanged dataframe
-
-# Display pandas interpretation of datatypes (int64, float64, object etc.)
-def data_types(df):
-    for column in df.columns:
-        print(column, '\t%s' % df[column].dtypes)
-
-# Number of unique values in each column (null-values exluded)
-def uniques(df):
-    # Initialize dictionary with column names
-    # all values set to 0
-    uniq = {c:0 for c in df.columns}
-    
-    # Iterate df by column
-    for column in df.columns:
-        # Add all column values into a list
-        x = [v for v in df[column]]
-        
-        # set = the unique values in the column
-        # len = the number of unique values in the column
-        # Update dict: key = column name, value = nr uniques
-        uniq[column] = len(set(x))
-    return uniq
-
-def analysis():    
-    # 1.
-    print('#1. Dataset overview\n')
-    print( overview(data.df) )
-    print('(...)')
-    
-    # 2.
-    print('\n#2. Dimensions')
-    shape = dimensions(data.df)
-    print('Rows: %d, Columns: %d' % (shape[0], shape[1]))
-    
-    # 3.
-    print('\n#3. Empty values\n')
-    df = null_values(data.df)
-    
-    # 4.
-    print('\n#4. Data types\n')
-    data_types(df)
-    uniq = uniques(df)
-    
-    # 5.
-    print('\n#5. Number of unique values in columns\n')
-    for k,v in uniq.items():
-        print(k, v)
-
-
 class column(object):
-    def __init__(self, name, dtype, level):
+    
+    def __init__(self, name, data, dtype, level = None):
         self.name = name
+        self.data = data
         self.dtype = dtype
         self.level = level
-
-    # This is where we suggest the datatype for each column
-    # for the user to specify in R
-    def suggested_type(df, cid):
-        # Check if we've been here before
-        #if all(item == '' for item in dt.values()):
-            
-    #        if(column == df.columns[cid]):
-                
-        for column in df.columns:
-                
-            if df[column].dtypes == 'object':
-                x = [v for v in df[column]]
-                dt[column] = 'factor'
-                print(column, '\tFactor w/ %d levels' % len(set(x)))
-            
-            elif df[column].dtypes == 'float64':
-                dt[column] = 'numeric'
-                print(column, '\tNumeric')
+        
+    def get_name(self):
+        return self.name
     
-          
-            elif df[column].dtypes == 'int64':
-                x = [v for v in df[column]]
-                unique = set(x)
-                su = (sorted(unique))
-                sequential = {i for i in range(su[0],su[0]+len(su))}
-                
-                if unique == sequential:
-                    dt[column] = 'factor'
-                    print(column, '\tFactor w/ %d levels' % len(set(x)))
-                    
-                else:
-                    dt[column] = 'numeric'
-                    print(column, '\tNumeric')
-            
-        #print(dt)            
-        ui = input('\nAre the suggested datatypes correct? (yes / no): ')
-        print()
-        def selection(df):
-            global dt
-            for i, column in enumerate(df.columns):
-                print(i+1, '\t%s' % column)
-        
-        if ui in ['no', 'No', 'NO', 'n', 'N']:
-            selection(df)
-            i = int(input('Change datatype (select a number): ')) - 1
-            print()
-            
-            
-            if i > 0 and i <= len(df.columns):
-                if dt[df.columns[i]] == 'numeric':
-                    dt[df.columns[i]] = 'factor'
-                    print('%s set to factor' % df.columns[i])
-                    
-                elif dt[df.columns[i]] == 'factor':
-                    dt[df.columns[i]] = 'numeric'
-                    print('%s set to numeric' % df.columns[i])
-                    
-            suggested_type(df, i)
-        
-            
-    def tree(df):
-        # Factor + Factor + Numeric
-        # Find highest level factor
-        # For each group in hlf, cbind 
-        dtypes = ([x for x in dt.values()])
-        #print('factor', dtypes.count('factor'))
-        #print('numeric', dtypes.count('numeric'))
-        #print(uniques(df))
-        
-        # Check dimensions
-        
-        if len(dtypes) == 1:
-            None
-            
-        elif len(dtypes) == 2:
-            
-            ## Check datatypes
-            
-            # Are both columns factors?
-            if all(value == 'factor' for value in dtypes):
-                
-                # Are both columns same level factors?
-                uniq = [value for value in uniques(df).values()]
-                
-                if uniq[0] == uniq[1]:
-                    
-                    # Pearson chi-square test
-                
-                    print()                    
-                    print('counts <- table(%s$%s, %s$%s)' %
-                         (data.varname, df.columns[0], data.varname, df.columns[1]))                    
-                    print('chisq.test(counts)')    
-                    print('fisher.test(counts)')                    
-                    print('library(DescTools)')
-                    print('GTest(counts)')
-        
-        elif len(dtypes) == 3:
-            
-            # Two factor, one numeric
-            if dtypes.count('factor') == 2 and dtypes.count('numeric') == 1:
-                
-                # Find numeric column
-                num = [k for k,v in dt.items() if v == 'numeric'][0]
-               # print(num)
-                
-                #print( df[num] )
-                iters = [iter(df[num])] * 2
-                z = zip(*iters)
-                
-                
-                
-                #cbind = [(start, stop) for start, stop in z]
-                #print(cbind)
-               # for start, stop in z:
-               #     print(start, stop)
-                
-               # for i in z:
-               #     print(i)
-                
-                #for item in df[num]:
-                 #   print(item)
+    def get_data(self):
+        return self.data
     
+    def get_dtype(self):
+        return self.dtype
+    
+    def get_level(self):
+        return self.level
+    
+    def set_data(self, new_data):
+        self.data = new_data
+        
+    def set_dtype(self, new_dtype):
+        self.dtype = new_dtype
+        
+    def set_level(self, new_level):
+        self.level = new_level
+        
+    def suggest_dtype(self):
+        if self.dtype == 'float64':
+            self.set_dtype('Numeric')
+        
+        elif self.dtype == 'int64':
+            unique = sorted(set(self.data))
+            sequential = [i for i in range(unique[0],unique[0]+len(unique))]
+            
+            if unique == sequential:
+                self.set_dtype('Factor')
+                self.set_level(len(unique))
+            else:
+                self.set_dtype('Numeric')
+        
+        elif self.dtype == 'object':
+            self.set_dtype('Factor')
+            self.set_level(len(set(self.data)))             
+        #self.summary()
+
+    def summary(self):
+        print('Name: ' + self.get_name())
+        print('Type: ' + str(self.get_dtype()))
+        print('Levels: ' + str(self.get_level()))
                 
-               # print('%s.test <- cbind(%s' % (varname, (x for x in cbind)))
+    def switch_dtype(self):
+        if self.get_dtype() == 'Factor':
+            self.set_dtype('Numeric')
+        else:
+            self.set_dtype('Factor')
     
-    
-
-
-
-
 if __name__ == '__main__':
-    analysis()
-    # 6. in progress
-    print('\n#6. Suggested type\n')
+    filename = 'tiger.csv'
+    df = pd.read_csv(filename, sep = ';')
     
-    #suggested_type(df,0)
+    cols = []
     
-    # 7. in progress
+    for c in df.columns:
+        tmp = column(c,df[c], df[c].dtypes)
+        cols.append(tmp)
     
-    #tree(df)
+    def check_dtype():
+        print('\tName\tType\tLevel')
+        for i,c in enumerate(cols):
+            c.suggest_dtype()
+            print('%d.\t%s\t%s\t%s' % (i+1, c.get_name(), c.get_dtype(), c.get_level()))
+
+        feedback = input('Are the datatypes correct? y/n: ')
+
+        if feedback in ['no', 'No', 'NO', 'n', 'N']:
+            feedback = input('Which column\'s datatype should be changed? (select number): ')
+            cols[int(feedback) - 1].switch_dtype()
+            print()
+            for i,c in enumerate(cols):
+                print('%d.\t%s\t%s\t%s' % (i+1, c.get_name(), c.get_dtype(), c.get_level()))
+            
+            feedback = input('Is there any other change to be made? y/n: ')
+            if feedback not in ['no', 'No', 'NO', 'n', 'N']:
+                check_dtype()
+                
+    check_dtype()
+
